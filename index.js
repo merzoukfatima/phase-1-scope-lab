9ec2b854e2c933d0be5372e573d3c207a6b339e1
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-08-07 02:14:16 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName='bob'
+var upperCaseCustomerName=()=>
+customerName=customerName.toLocaleUpperCase()
+customerName='bob'
+
+var setBestCustomer=()=>    
+ bestCustomer = 'not bob';
+//console.log(bestCustomer)
+
+
+var overwriteBestCustomer=(name)=>{
+setBestCustomer()
+bestCustomer=name
+}
+const leastFavoriteCustomer ="me"
+
+var changeLeastFavoriteCustomer = ()=>leastFavoriteCustomer="he"
+changeLeastFavoriteCustomer()
